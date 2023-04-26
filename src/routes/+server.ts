42af437043dd9566457908a3209d7db5f437f191
Yanik Ammann <confused@ant.lgbt>
2023-04-26 11:46:29 +0200

    switch to static redirect

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,3 +1,3 @@
 import type { RequestHandler } from './$types'
 
-export const GET: RequestHandler = ({ request }) => Response.redirect(`${request.url}-3`)
+export const GET: RequestHandler = () => Response.redirect(`https://stash.hera.lan/-3`)
