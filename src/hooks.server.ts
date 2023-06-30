f80eb8becd78d7908d9bfded40d012239f2c6fcf
Yanik Ammann <confused@ant.lgbt>
2023-06-30 18:40:14 +0200

    fix origin protocol

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -17,6 +17,8 @@ export const handle: Handle = (async ({ event, resolve }) => {
   if (event.url.origin == "http://sveltekit-prerender")
     return await resolve(event)
 
+  const origin = event.url.origin.includes("localhost") ? event.url.origin : event.url.origin.replace("http", "https")
+
   const sessionCookie = event.cookies.get("session") || ""
 
   if (event.url.pathname.startsWith("/guest")) {
@@ -61,15 +63,15 @@ export const handle: Handle = (async ({ event, resolve }) => {
 
     // redirect to auth
     if (!isValid && !event.url.pathname.startsWith("/auth"))
-      return Response.redirect(`${event.url.origin}/auth`, 307)
+      return Response.redirect(`${origin}/auth`, 307)
 
     // if has logged in, return to main page
     if (isValid && event.url.pathname == "/auth")
-      return Response.redirect(event.url.origin, 307)
+      return Response.redirect(origin, 307)
   }
 
   if (event.url.pathname == "/auth" && sessionCookie == "TotallySecretToken@4x2PLm6J")
-    return Response.redirect(`${event.url.origin}/guest`, 307)
+    return Response.redirect(`${origin}/guest`, 307)
 
   return await resolve(event)
 })
--- src/routes/guest/+server.ts
+++ src/routes/guest/+server.ts
@@ -1,3 +1,3 @@
 import type { RequestHandler } from './$types'
 
-export const GET: RequestHandler = ({ url }) => Response.redirect(`${url.origin}/guest/-9`)
+export const GET: RequestHandler = ({ url }) => Response.redirect(`${url.origin.includes("localhost") ? url.origin : url.origin.replace("http", "https")}/guest/-9`)
