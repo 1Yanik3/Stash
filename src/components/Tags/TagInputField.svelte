01f310d621b711771973abd90b6a97f1bc24dd76
Yanik Ammann <confused@ant.lgbt>
2024-01-11 14:37:29 +0100

    feat: add fuzzy search for tags and refactor tagging system

---
+++ src/components/Tags/TagInputField.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+  import type { PageData } from "../../routes/[cluster]/$types"
+  import { page } from "$app/stores"
+  import { createEventDispatcher } from "svelte"
+  import FuzzySearch from "fuzzy-search"
+
+  let value: string
+  let focused = false
+
+  let selectionIndex = -1
+
+  const dispatch = createEventDispatcher()
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case "ArrowUp":
+        if (selectionIndex > 0) selectionIndex--
+        break
+      case "ArrowDown":
+        if (selectionIndex <= results.length) selectionIndex++
+        break
+      case "Enter":
+        if (selectionIndex >= 0) dispatch("selected", results[selectionIndex])
+        else dispatch("selected", value)
+        break
+      default:
+        selectionIndex = -1
+        break
+    }
+  }
+
+  $: searcher = new FuzzySearch(($page.data as PageData).tags, ["tag"], {
+    caseSensitive: false,
+    sort: true
+  })
+  $: results = searcher
+    .search(value)
+    .slice(0, 10)
+    .map(t => t.tag.join("/"))
+</script>
+
+<main>
+  <input
+    type="text"
+    on:focusin={() => (focused = true)}
+    on:focusout={() => (focused = false)}
+    bind:value
+    on:keydown|stopPropagation={handleKeyDown}
+  />
+
+  {#if value?.length > 0 && focused}
+    <div class="results">
+      {#each results as result, i}
+        <!-- TODO: Add count -->
+        <span class:active={i == selectionIndex}>{result}</span>
+      {/each}
+    </div>
+  {/if}
+</main>
+
+<style lang="scss">
+  $height: 25px;
+  $width: 13em;
+
+  main {
+    position: relative;
+
+    input {
+      height: $height;
+      width: $height;
+      transition: width 200ms;
+
+      &:focus {
+        width: $width;
+      }
+    }
+
+    .results {
+      position: absolute;
+      top: $height + 8px;
+      left: 0;
+
+      display: grid;
+
+      background: $color-dark-level-1;
+      border: 1px solid $border-color-1;
+
+      span {
+        display: block;
+        padding: 0.5em;
+
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        transition: background 150ms;
+
+        &.active {
+          background: #303030;
+        }
+      }
+    }
+  }
+</style>
--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -1,112 +1,106 @@
 <script lang="ts">
-    import { browser } from "$app/environment";
-    import { page } from "$app/stores";
-    import {
-        controller,
-        detailsVisible,
-        imageSuffixParameter,
-        isFullscreen,
-        settings,
-        visibleMedium,
-    } from "$lib/stores";
-    import selectFiles from "select-files";
-    import Shortcut from "../reusables/Shortcut.svelte";
-    import type { PageData } from "../routes/[cluster]/$types";
-    import Icon from "./Icon.svelte";
-    import { invalidate } from "$app/navigation";
-    // import UpscalePopup from './Popups/UpscalePopup.svelte';
-    $: pageData = $page.data as PageData;
+  import { browser } from "$app/environment"
+  import { page } from "$app/stores"
+  import {
+    controller,
+    detailsVisible,
+    imageSuffixParameter,
+    isFullscreen,
+    settings,
+    visibleMedium
+  } from "$lib/stores"
+  import selectFiles from "select-files"
+  import Shortcut from "../reusables/Shortcut.svelte"
+  import type { PageData } from "../routes/[cluster]/$types"
+  import Icon from "./Icon.svelte"
+  import { invalidate } from "$app/navigation"
+  import TagInputField from "./Tags/TagInputField.svelte"
+  // import UpscalePopup from './Popups/UpscalePopup.svelte';
+  $: pageData = $page.data as PageData
 
-    const handleKeyDown = (e: KeyboardEvent) => {
-        const value: string = (e.target as any).value;
-        if (e.key == "Enter" && value) {
-            fetch(`/api/media/${$visibleMedium?.id}/tag`, {
-                method: "PUT",
-                body: JSON.stringify({
-                    name: value,
-                }),
-            })
-                .then(() => {
-                    const isInUnsorted =
-                        $visibleMedium?.tags.length == 1 &&
-                        $visibleMedium?.tags[0] == "show_unsorted";
+  const addTagToMedia = (value: string) => {
+    fetch(`/api/media/${$visibleMedium?.id}/tag`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: value
+      })
+    })
+      .then(() => {
+        const isInUnsorted =
+          $visibleMedium?.tags.length == 1 &&
+          $visibleMedium?.tags[0] == "show_unsorted"
 
-                    const tmp = $visibleMedium;
-                    tmp?.tags.push(value);
-                    visibleMedium.set(tmp);
+        const tmp = $visibleMedium
+        tmp?.tags.push(value)
+        visibleMedium.set(tmp)
 
-                    if (isInUnsorted) {
-                        removeTagFromMedia("show_unsorted");
-                    } else {
-                        invalidate("media-and-tags");
-                    }
-                })
-                .catch(console.error);
+        if (isInUnsorted) {
+          removeTagFromMedia("show_unsorted")
+        } else {
+          invalidate("media-and-tags")
         }
-    };
+      })
+      .catch(console.error)
+  }
 
-    const removeTagFromMedia = (tag: string) => {
-        fetch(`/api/media/${$visibleMedium?.id}/tag/${tag}`, {
-            method: "DELETE",
-        }).then(() => {
-            if (!$visibleMedium) return;
-            const tmp = $visibleMedium;
-            tmp.tags = tmp.tags.filter((t) => t != tag);
-            visibleMedium.set(tmp);
+  const removeTagFromMedia = (tag: string) => {
+    fetch(`/api/media/${$visibleMedium?.id}/tag/${tag}`, {
+      method: "DELETE"
+    }).then(() => {
+      if (!$visibleMedium) return
+      const tmp = $visibleMedium
+      tmp.tags = tmp.tags.filter(t => t != tag)
+      visibleMedium.set(tmp)
 
-            invalidate("media-and-tags");
-        });
-    };
+      invalidate("media-and-tags")
+    })
+  }
 
-    (window as any).test = () =>
-        imageSuffixParameter.set(
-            `?${Math.random().toString(16).substring(2, 8)}`
-        );
-    const replaceMedia = (newMedia: Blob) => {
-        const data = new FormData();
-        data.append(
-            "file",
-            new File([newMedia], $visibleMedium?.name || "newImage.jpg", {
-                type: newMedia.type,
-            })
-        );
+  ;(window as any).test = () =>
+    imageSuffixParameter.set(`?${Math.random().toString(16).substring(2, 8)}`)
+  const replaceMedia = (newMedia: Blob) => {
+    const data = new FormData()
+    data.append(
+      "file",
+      new File([newMedia], $visibleMedium?.name || "newImage.jpg", {
+        type: newMedia.type
+      })
+    )
 
-        fetch(`/api/media/${$visibleMedium?.id}/replace`, {
-            method: "PUT",
-            body: data,
-        }).then(async () => {
-            // reload image in viewer
-            imageSuffixParameter.set(
-                `?${Math.random().toString(16).substring(2, 8)}`
-            );
+    fetch(`/api/media/${$visibleMedium?.id}/replace`, {
+      method: "PUT",
+      body: data
+    }).then(async () => {
+      // reload image in viewer
+      imageSuffixParameter.set(`?${Math.random().toString(16).substring(2, 8)}`)
 
-            // visibleMedium.update()
-            // // This seems to ignore the history pushes
-            // window.location.reload()
-        });
-    };
+      // visibleMedium.update()
+      // // This seems to ignore the history pushes
+      // window.location.reload()
+    })
+  }
 
-    const replaceWithLocalMedia = () => {
-        if (!browser) return;
+  const replaceWithLocalMedia = () => {
+    if (!browser) return
 
-        selectFiles({}).then((files) => {
-            if (files && files[0]) replaceMedia(files[0]);
-        });
-    };
+    selectFiles({}).then(files => {
+      if (files && files[0]) replaceMedia(files[0])
+    })
+  }
 
-    const replaceThumbnail = () => {
-        if (!browser) return;
+  const replaceThumbnail = () => {
+    if (!browser) return
 
-        $controller.setPopup("Replace Video Thumbnail");
-    };
+    $controller.setPopup("Replace Video Thumbnail")
+  }
 </script>
 
 <!-- Toggle Fullscreen -->
 <Shortcut
-    key="f"
-    action={() => {
-        isFullscreen.set(!$isFullscreen);
-    }}
+  key="f"
+  action={() => {
+    isFullscreen.set(!$isFullscreen)
+  }}
 />
 
 <!-- {#key $visibleMedium}
@@ -114,148 +108,138 @@
 {/key} -->
 
 <main
-    class:fullscreen={$isFullscreen}
-    class:windowControlsSpacer={$settings.windowControlsSpacer}
+  class:fullscreen={$isFullscreen}
+  class:windowControlsSpacer={$settings.windowControlsSpacer}
 >
-    <section>
-        <button on:click={() => visibleMedium.set(null)}>
-            <Icon name="mdiClose" size={0.8} />
-        </button>
+  <section>
+    <button on:click={() => visibleMedium.set(null)}>
+      <Icon name="mdiClose" size={0.8} />
+    </button>
 
-        <button
-            on:click={() => {
-                isFullscreen.set(!$isFullscreen);
-                if ($isFullscreen) {
-                    document.documentElement.requestFullscreen();
-                } else {
-                    document.exitFullscreen();
-                }
-            }}
-        >
-            <Icon name="mdiFullscreen" size={0.8} />
-        </button>
+    <button
+      on:click={() => {
+        isFullscreen.set(!$isFullscreen)
+        if ($isFullscreen) {
+          document.documentElement.requestFullscreen()
+        } else {
+          document.exitFullscreen()
+        }
+      }}
+    >
+      <Icon name="mdiFullscreen" size={0.8} />
+    </button>
 
-        <button on:click={() => detailsVisible.set(!$detailsVisible)}>
-            <Icon name="mdiInformationOutline" size={0.8} />
-        </button>
-    </section>
+    <button on:click={() => detailsVisible.set(!$detailsVisible)}>
+      <Icon name="mdiInformationOutline" size={0.8} />
+    </button>
+  </section>
 
-    <div>
-        {#each $visibleMedium?.tags || [] as tag}
-            <span
-                class="tag"
-                on:contextmenu|preventDefault={() => removeTagFromMedia(tag)}
-                >{tag}</span
-            >
-        {/each}
-        {#if pageData.cluster.type != "collection" || $visibleMedium?.tags.length != 1}
-            <input type="text" on:keydown|stopPropagation={handleKeyDown} />
-        {/if}
-    </div>
+  <div>
+    {#each $visibleMedium?.tags || [] as tag}
+      <span
+        class="tag"
+        on:contextmenu|preventDefault={() => removeTagFromMedia(tag)}
+        >{tag}</span
+      >
+    {/each}
+    {#if pageData.cluster.type != "collection" || $visibleMedium?.tags.length != 1}
+      <TagInputField on:selected={({ detail }) => addTagToMedia(detail)} />
+    {/if}
+  </div>
 
-    <section>
-        <button on:click={replaceWithLocalMedia} title="Replace file content">
-            <Icon name="mdiFileReplace" size={0.8} />
-        </button>
+  <section>
+    <button on:click={replaceWithLocalMedia} title="Replace file content">
+      <Icon name="mdiFileReplace" size={0.8} />
+    </button>
 
-        <!-- TODO: Timestamp picker (in frontend, using video element) -->
-        {#if $visibleMedium?.type.startsWith("video")}
-            <button on:click={replaceThumbnail} title="Replace file thumbnail">
-                <Icon name="mdiFileReplaceOutline" size={0.8} />
-            </button>
-        {/if}
+    <!-- TODO: Timestamp picker (in frontend, using video element) -->
+    {#if $visibleMedium?.type.startsWith("video")}
+      <button on:click={replaceThumbnail} title="Replace file thumbnail">
+        <Icon name="mdiFileReplaceOutline" size={0.8} />
+      </button>
+    {/if}
 
-        <!-- <button on:click={() => upscalePopup_open = true}>
+    <!-- <button on:click={() => upscalePopup_open = true}>
             <Icon name="mdiResize" size={0.8}/>
         </button> -->
 
-        <button
-            on:click={() =>
-                window.open(
-                    `${$page.data.serverURL}/file/${$visibleMedium?.id}`,
-                    "_blank"
-                )}
-        >
-            <Icon name="mdiOpenInNew" size={0.8} />
-        </button>
-    </section>
+    <button
+      on:click={() =>
+        window.open(
+          `${$page.data.serverURL}/file/${$visibleMedium?.id}`,
+          "_blank"
+        )}
+    >
+      <Icon name="mdiOpenInNew" size={0.8} />
+    </button>
+  </section>
 </main>
 
 <style lang="scss">
-    main {
-        border-left: 1px solid $border-color-base;
-        border-bottom: 1px solid $border-color-base;
-        padding: 0.35em;
-        min-width: calc(100% - 4em);
-        background: $color-dark-level-1;
+  main {
+    border-left: 1px solid $border-color-base;
+    border-bottom: 1px solid $border-color-base;
+    padding: 0.35em;
+    min-width: calc(100% - 4em);
+    background: $color-dark-level-1;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    div {
+      display: flex;
+      .tag {
+        background: $color-dark-level-2;
+        padding: 0.3em 0.5em;
+        margin: 0.15em;
+        border: 1px solid $color-dark-level-1;
+        border-radius: 3px;
+
+        margin-right: 0.25em;
+
+        cursor: pointer;
+      }
+    }
+
+    section {
+      display: flex;
+      align-items: center;
+
+      div {
+        height: 30px;
+        width: 30px;
 
         display: flex;
-        justify-content: space-between;
         align-items: center;
+        justify-content: center;
 
-        div {
-            display: flex;
-            .tag {
-                background: $color-dark-level-2;
-                padding: 0.3em 0.5em;
-                margin: 0.15em;
-                border: 1px solid $color-dark-level-1;
-                border-radius: 3px;
+        cursor: pointer;
+      }
 
-                margin-right: 0.25em;
-
-                cursor: pointer;
-            }
-            input {
-                margin-left: 0.25em;
-                width: 2em;
-
-                transition: width 200ms;
-
-                &:focus {
-                    width: 7em;
-                }
-            }
-        }
-
-        section {
-            display: flex;
-            align-items: center;
-
-            div {
-                height: 30px;
-                width: 30px;
-
-                display: flex;
-                align-items: center;
-                justify-content: center;
-
-                cursor: pointer;
-            }
-
-            button {
-                outline: none;
-            }
-        }
-
-        &.fullscreen {
-            box-shadow: none;
-            position: absolute;
-            z-index: 5;
-            left: 0;
-            &.windowControlsSpacer {
-                left: 4.625em;
-            }
-            top: 0;
-
-            div,
-            section:last-child,
-            button:not(:nth-child(2)) {
-                display: none;
-            }
-
-            background: none;
-            border-bottom: none;
-        }
+      button {
+        outline: none;
+      }
     }
+
+    &.fullscreen {
+      box-shadow: none;
+      position: absolute;
+      z-index: 5;
+      left: 0;
+      &.windowControlsSpacer {
+        left: 4.625em;
+      }
+      top: 0;
+
+      div,
+      section:last-child,
+      button:not(:nth-child(2)) {
+        display: none;
+      }
+
+      background: none;
+      border-bottom: none;
+    }
+  }
 </style>
