7522fd62a232afffac9f9ef2258a88ce9cb62d20
Yanik Ammann <confused@ant.lgbt>
2024-02-10 13:32:14 +0100

    style(ClusterSection.svelte): improve mobile layout for sidebar sections

--- src/routes/[cluster]/ClusterSection.svelte
+++ src/routes/[cluster]/ClusterSection.svelte
@@ -1,159 +1,160 @@
 <script lang="ts">
-    import {
-        activeSortingMethod,
-        activeSetMethod,
-        controller,
-        settings,
-        traverse,
-    } from "$lib/stores";
-    import { invalidate } from "$app/navigation";
-    import { page } from "$app/stores";
-    import { setMethods, sortingMethods } from "../../types";
+  import {
+    activeSortingMethod,
+    activeSetMethod,
+    controller,
+    settings,
+    traverse
+  } from "$lib/stores"
+  import { invalidate } from "$app/navigation"
+  import { page } from "$app/stores"
+  import { setMethods, sortingMethods } from "../../types"
 
-    import type { LayoutData } from "./$types";
-    import SidebarButton from "./SidebarButton.svelte";
-    $: pageData = $page.data as LayoutData;
+  import type { LayoutData } from "./$types"
+  import SidebarButton from "./SidebarButton.svelte"
+  $: pageData = $page.data as LayoutData
 </script>
 
 <main
-    class:mobile={$settings.mobileLayout}
-    class:windowControlsSpacer={$settings.windowControlsSpacer}
+  class:mobile={$settings.mobileLayout}
+  class:windowControlsSpacer={$settings.windowControlsSpacer}
 >
-    <section>
-        {#if $settings.windowControlsSpacer}
-            <span style="height: 1.5em; pointer-events: none" />
-        {/if}
+  <section>
+    {#if $settings.windowControlsSpacer}
+      <span style="height: 1.5em; pointer-events: none" />
+    {/if}
 
-        <SidebarButton
-            hidden={!$settings.mobileLayout}
-            disabled={["collection", "stories"].includes(pageData.cluster.type)}
-            on:click={() => {
-                activeSortingMethod.set(
-                    sortingMethods[
-                        (sortingMethods.indexOf($activeSortingMethod) + 1) %
-                            sortingMethods.length
-                    ]
-                );
-                invalidate("media-and-tags");
-            }}
-            on:contextmenu={({ detail }) => {
-                detail.preventDefault();
-                invalidate("media-and-tags");
-            }}
-            icon={$activeSortingMethod.icon}
-        >
-            Sorting Method
-        </SidebarButton>
+    <SidebarButton
+      hidden={!$settings.mobileLayout}
+      disabled={["collection", "stories"].includes(pageData.cluster.type)}
+      on:click={() => {
+        activeSortingMethod.set(
+          sortingMethods[
+            (sortingMethods.indexOf($activeSortingMethod) + 1) %
+              sortingMethods.length
+          ]
+        )
+        invalidate("media-and-tags")
+      }}
+      on:contextmenu={({ detail }) => {
+        detail.preventDefault()
+        invalidate("media-and-tags")
+      }}
+      icon={$activeSortingMethod.icon}
+    >
+      Sorting Method
+    </SidebarButton>
 
-        <SidebarButton
-            hidden={!$settings.mobileLayout}
-            disabled={pageData.cluster.type == "stories"}
-            on:click={() => {
-                traverse.set(!$traverse);
-                invalidate("media-and-tags");
-            }}
-            icon={$traverse ? "mdiHook" : "mdiHookOff"}
-        >
-            Traverse
-        </SidebarButton>
+    <SidebarButton
+      hidden={!$settings.mobileLayout}
+      disabled={pageData.cluster.type == "stories"}
+      on:click={() => {
+        traverse.set(!$traverse)
+        invalidate("media-and-tags")
+      }}
+      icon={$traverse ? "mdiHook" : "mdiHookOff"}
+    >
+      Traverse
+    </SidebarButton>
 
-        <SidebarButton
-            hidden={!$settings.mobileLayout}
-            disabled={["collection", "stories"].includes(pageData.cluster.type)}
-            on:click={() => {
-                activeSetMethod.set(
-                    setMethods[
-                        (setMethods.indexOf($activeSetMethod) + 1) %
-                            setMethods.length
-                    ]
-                );
-                invalidate("media-and-tags");
-            }}
-            icon={$activeSetMethod.icon}
-        >
-            Set Method ({$activeSetMethod.title})
-        </SidebarButton>
-    </section>
+    <SidebarButton
+      hidden={!$settings.mobileLayout}
+      disabled={["collection", "stories"].includes(pageData.cluster.type)}
+      on:click={() => {
+        activeSetMethod.set(
+          setMethods[
+            (setMethods.indexOf($activeSetMethod) + 1) % setMethods.length
+          ]
+        )
+        invalidate("media-and-tags")
+      }}
+      icon={$activeSetMethod.icon}
+    >
+      Set Method ({$activeSetMethod.title})
+    </SidebarButton>
+  </section>
 
-    <section>
-        {#each pageData.clusters.sort((a, b) => a.sortOrder - b.sortOrder) as c}
-            <SidebarButton
-                hidden={!$settings.mobileLayout}
-                iconNoTyping={c.icon}
-                href="/{c.name}"
-                active={c.id == pageData.cluster.id}
-            >
-                {c.name}
-            </SidebarButton>
-        {/each}
-    </section>
+  <section>
+    {#each pageData.clusters.sort((a, b) => a.sortOrder - b.sortOrder) as c}
+      <SidebarButton
+        hidden={!$settings.mobileLayout}
+        iconNoTyping={c.icon}
+        href="/{c.name}"
+        active={c.id == pageData.cluster.id}
+      >
+        {c.name}
+      </SidebarButton>
+    {/each}
+  </section>
 
-    <section>
-        {#if !$settings.mobileLayout}
-            <SidebarButton
-                hidden
-                icon="mdiKeyboard"
-                on:click={() => $controller.setPopup("Shortcuts")}
-            />
-        {/if}
+  <section>
+    {#if !$settings.mobileLayout}
+      <SidebarButton
+        hidden
+        icon="mdiKeyboard"
+        on:click={() => $controller.setPopup("Shortcuts")}
+      />
+    {/if}
 
-        <SidebarButton
-            hidden={!$settings.mobileLayout}
-            icon="mdiCog"
-            on:click={() => $controller.setPopup("Settings")}
-        >
-            Settings
-        </SidebarButton>
-    </section>
+    <SidebarButton
+      hidden={!$settings.mobileLayout}
+      icon="mdiCog"
+      on:click={() => $controller.setPopup("Settings")}
+    >
+      Settings
+    </SidebarButton>
+  </section>
 </main>
 
 <style lang="scss">
-    main {
-        display: grid;
-        justify-content: center;
-        align-items: center;
-        grid-auto-rows: 1fr;
+  main {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    grid-auto-rows: 1fr;
 
-        &:not(.mobile) {
-            & > :first-child {
-                align-self: start;
-            }
-            & > :last-child {
-                align-self: end;
-            }
-        }
-
-        padding-top: 0.5em;
-        padding-bottom: 0.5em;
-
-        border-right: 1px solid $border-color-base;
-
-        width: 64px;
-        &.windowControlsSpacer {
-            width: 77px;
-            padding: 1em 0;
-        }
-
-        section {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        -webkit-app-region: drag;
-
-        span.disabled {
-            pointer-events: none;
-            filter: opacity(0.5);
-        }
-
-        &.mobile {
-            width: 100%;
-            border-right: none;
-            display: flex;
-            flex-direction: column;
-            gap: 1em;
-        }
+    &:not(.mobile) {
+      & > :first-child {
+        align-self: start;
+      }
+      & > :last-child {
+        align-self: end;
+      }
     }
+
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+
+    border-right: 1px solid $border-color-base;
+
+    width: 64px;
+    &.windowControlsSpacer {
+      width: 77px;
+      padding: 1em 0;
+    }
+
+    section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+
+    -webkit-app-region: drag;
+
+    span.disabled {
+      pointer-events: none;
+      filter: opacity(0.5);
+    }
+
+    &.mobile {
+      width: 100%;
+      border-right: none;
+      display: block;
+      gap: 1em;
+      section {
+        display: block;
+      }
+    }
+  }
 </style>
