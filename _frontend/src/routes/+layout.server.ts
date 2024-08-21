8575c718ebf6ef8531b97415f50732caca5b2cd1
Yanik Ammann <confused@ant.lgbt>
2024-08-21 19:11:13 +0200

    fix: remove tag items from layout

--- _frontend/src/routes/+layout.server.ts
+++ _frontend/src/routes/+layout.server.ts
@@ -40,25 +40,12 @@ const getClusters = async (token: string) => {
 export const load: LayoutServerLoad = async ({ url, cookies }) => {
   console.log(new Date().toISOString(), "/+layout.server.ts1", url.pathname)
 
-  let tagIcons = {} as { [icon in keyof typeof possibleIcons]: string[] }
-  ;(
-    (await prisma.tagIcons.findMany({
-      orderBy: {
-        tag: "asc"
-      }
-    })) as { icon: keyof typeof possibleIcons; tag: string }[]
-  ).forEach(({ icon, tag }) => {
-    if (!tagIcons[icon]) tagIcons[icon] = []
-    tagIcons[icon].push(tag)
-  })
-
   let serverURL = "https://stash.hera.lan"
   if (url.hostname == "stash.any.gay") serverURL = "https://stash.any.gay"
   console.log(new Date().toISOString(), "/+layout.server.ts2", url.pathname)
 
   return {
     clusters: await getClusters(cookies.get("session") || ""),
-    tagIcons,
     serverURL
   }
 }
