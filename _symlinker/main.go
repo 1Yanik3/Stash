5f939fcd4d76e65705b4a399a317b1cceaa8cad4
Yanik Ammann <confused@ant.lgbt>
2024-10-16 21:15:39 +0200

    feat(symlinker): ignore images and add .mp4 ending

--- _symlinker/main.go
+++ _symlinker/main.go
@@ -25,9 +25,14 @@ func main() {
 	}
 	defer db.Close()
 
+	// Delete old links
+	if err := os.RemoveAll("/hierarchy"); err != nil {
+		log.Fatal(err)
+	}
+
 	// Execute the query
 	query := `
-        SELECT t.id, t.tag, m.id, m.name 
+        SELECT t.id, t.tag, m.id, m.name, m.type 
         FROM "Tags" t 
         LEFT JOIN "_MediaToTags" tm ON t.id = tm."B" 
         RIGHT JOIN "Media" m ON tm."A" = m.id
@@ -45,13 +50,18 @@ func main() {
 		var tagName string
 		var mediaID string
 		var mediaName string
+		var mediaType string
 
-		if err := rows.Scan(&tagID, &tagName, &mediaID, &mediaName); err != nil {
+		if err := rows.Scan(&tagID, &tagName, &mediaID, &mediaName, &mediaType); err != nil {
 			log.Fatal(err)
 		}
 
+		if mediaType != "video" {
+			continue
+		}
+
 		// Create the directory structure
-		dirPath := fmt.Sprintf("/hierarchy/%s_%d", tagName, tagID)
+		dirPath := fmt.Sprintf("/hierarchy/%s_%d.mp4", tagName, tagID)
 		if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
 			log.Fatal(err)
 		}
